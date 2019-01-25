2eaf3675c38997604bd6f52a44c171f6aeeeb2b2
markhannel <mdh386@nyu.edu>
2019-01-24 23:56:32 -0500

    Collected all pairwise routes between citibike stations while only saving the duration and start/end citibike dock station ids. This will allow me to query the approximate time it takes to travel between citibike docks.

--- src/data_collection/citibike_station_to_station.py
+++ src/data_collection/citibike_station_to_station.py
@@ -21,21 +21,22 @@ def main():
     # Load Citi Bike station GPS information.
     stations = '../../data/citibike/citibike_stations.csv'
     df = pd.read_csv(stations)
-    result_fn = '/media/mark/TOSHIBA EXT1/Projects/InAPinch/data/citibike/all_stations.csv'
+    result_fn = '/media/mark/TOSHIBA EXT1/Projects/InAPinch/data/citibike/all_stations_durations_docks.csv'
     
     # Form all pairs.
     count = 0
     with open(result_fn, 'a+') as f:
-        f.write("lat1\tlong1\tlat2\tlong2\tresponse\n")        
-        for pair in itertools.product(df[['latitude','longitude']].values,
-                                      df[['latitude','longitude']].values):
+        f.write("dock_id1\tdock_id2\tduration\n")        
+        for pair in itertools.product(df[['latitude','longitude', 'id']].values,
+                                      df[['latitude','longitude', 'id']].values):
             if not count % 1000:
                 print(count)
             if not np.array_equal(pair[0], pair[1]):
                 url = "http://127.0.0.1:5000/route/v1/bicycle/{},{};{},{}?steps=true"
                 url = url.format(pair[0][1], pair[0][0], pair[1][1], pair[1][0])
-                r = requests.get(url)
-                r = '{}\t{}\t{}\t{}\t{}\n'.format(pair[0][1], pair[0][0], pair[1][1], pair[1][0], r.content)
+                r = requests.get(url).json()
+                r = r['routes'][0]['duration']
+                r = '{}\t{}\t{}\n'.format(pair[0][2], pair[1][2], r)
                 f.write(r)
                 count += 1
             
