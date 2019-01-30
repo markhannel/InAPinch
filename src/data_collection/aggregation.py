63df337facc41641e656be40819f4549d0773e85
markhannel <mdh386@nyu.edu>
2019-01-29 23:24:35 -0500

    Fixing the 12 hr debacle.

--- src/data_collection/aggregation.py
+++ src/data_collection/aggregation.py
@@ -27,13 +27,17 @@ def main():
 
 
     # Fix the pm debacle.
-    df.loc[df['hour'] == 12, 'pm'] = df.loc[df['hour'] == 12, 'pm'].apply(lambda x: (x+1)%2)
-        
-    # Clean the data.
-    df['newhour'] = (df.hour + 12*df.pm) % 24
-    df['newhour'] = df['newhour'].apply(lambda x: '{:02}'.format(x))
+    df['date'] = pd.to_datetime(df['date'], format="%y-%m-%d")
+    mask = (df.hour==12) & (df.pm==1)
+    df.loc[mask, 'date'] += pd.DateOffset(days=1)
+    df.loc[mask, 'hour'] -= 12
+    df.loc[mask, 'pm'] -= 1
+    df.loc[~mask, 'hour'] = df.loc[~mask,'hour'] + 12*df.loc[~mask, 'pm']
+
+    df['newhour'] = df.hour.apply(lambda x: '{:02}'.format(x))
     df['newmin'] = df.minute.apply(lambda x: '{:02}'.format(x))
-    df['time'] = df.date + '-' + df.newhour + ':' + df.newmin
+    df['time'] = df['date'].astype(str) + '-' + df.newhour + ':' + df.newmin
+    
     df.time = pd.to_datetime(df.time, format="%y-%m-%d-%H:%M")
     df = df.drop(columns=['date', 'hour', 'minute', 'pm', 'newhour', 'newmin'])
 
