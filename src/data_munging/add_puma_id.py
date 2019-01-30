4c5fde9bb538b1c2c1f6fd573a587e7a06596273
markhannel <mdh386@nyu.edu>
2019-01-30 10:29:55 -0500

    Missing import of pickle module.

--- src/data_munging/add_puma_id.py
+++ src/data_munging/add_puma_id.py
@@ -12,6 +12,7 @@ import shapely.geometry
 import json
 import numpy as np
 import os
+import pickle
 
 def add_puma(df,lat_key='latitude', lon_key='longitude', id_key='id'):
     '''Create puma dictionary and apply it to the dataframe.
@@ -31,33 +32,37 @@ def add_puma(df,lat_key='latitude', lon_key='longitude', id_key='id'):
     # Create dock dictionary.
     dock_dict = {}
     for i, feature in enumerate(puma_geo['features']):
-        print(i)
         puma_id = feature['properties']['puma']
+        print(i, puma_id)
         shape = shapely.geometry.asShape(feature['geometry'])
         for dock in docks:
             point, dock_id = dock
-            if shape.contains(point):
-                dock_dict.update({dock_id:int(puma_id)})
-                docks.remove(dock)
-
+            if point.within(shape):
+                dock_dict.update({int(dock_id):int(puma_id)})
+                
     # Apply dictionary to dataframe.
     df['PUMA'] = df[id_key].map(dock_dict)
-    return df
+
+    return df, dock_dict
 
 def main():
     # Load historical station data.
     fn = '../../data/citibike/dock_historical/historical_data_puma.csv'
     if not os.path.exists(fn):
         station_df = pd.read_csv('../../data/citibike/dock_historical/historical_data.csv', index_col=0)
-        new_station_df = add_puma(station_df,'_long', '_lat', 'dock_id')
+        new_station_df, new_dict = add_puma(station_df,'_long', '_lat', 'dock_id')
         new_station_df.to_csv(fn)
+        with open('../../data/citibike/dock_dict_hist.pickle', "wb") as f:
+            pickle.dump(new_dict, f)
     
     # Load live station information.
     fn = '../../data/citibike/citibike_stations_puma.csv'
     if not os.path.exists(fn):
         station_df = pd.read_csv('../../data/citibike/citibike_stations.csv', index_col=0)
-        new_station_df = add_puma(station_df)
+        new_station_df, new_dict = add_puma(station_df)
         new_station_df.to_csv(fn)
+        with open('../../data/citibike/dock_dict_station.pickle', "wb") as f:
+            pickle.dump(new_dict, f)
     
 if __name__ == '__main__':
     main()
