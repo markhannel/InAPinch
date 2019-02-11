e3df5ede342b2d845282e212afff00e39267333a
markhannel <mdh386@nyu.edu>
2019-02-11 00:03:58 +0000

    Restructuring of website. Removed bloat from blog and am now rendering the app from the home page.

--- src/website/app/main/routes.py
+++ src/website/app/main/routes.py
@@ -7,42 +7,54 @@ from wtforms_components import TimeField
 from wtforms.validators import DataRequired
 from app.main import bp
 from datetime import datetime
+import requests
+from citibike_only_copy import SearchRoutes, get_directions
+import pandas as pd
+
+# Global variables for route information.
+CB_INFO_URL = "https://gbfs.citibikenyc.com/gbfs/es/station_information.json"
+CB_STATUS_URL = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
+OSRM_URL = 'https://127.0.0:5000/route/v1/bicycle/{},{};{},{}?steps=true'
+SR = SearchRoutes()
+#ROUTES = pd.read_csv('../../data/citibike/all_stations_docks.csv', sep='\t')
+
+# Get Station information.
+r = requests.get(CB_INFO_URL).json()
+STATION_INFO = pd.DataFrame(r['data']['stations'])
+
 
 @bp.route('/')
-@bp.route('/home')
+@bp.route('/home', methods=['POST','GET'])
 def home():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('home.html', title='Home', user=user, posts=posts)
+
+    form = ExampleForm()
+    if form.validate_on_submit():
+        # Get latest Citi Bike info.
+        cb_req = requests.get(CB_STATUS_URL).json()
+        
+        # Pick a route.
+        r = SR.start_to_end([form.start_lat.data,
+                             form.start_long.data],
+                             [form.end_lat.data,
+                              form.end_long.data])
+
+
+        
+        start_station = STATION_INFO.query('station_id == "{}"'.format(r[1])).name.values[0]
+        end_station   = STATION_INFO.query('station_id == "{}"'.format(r[2])).name.values[0]
+
+        # Get route directions.
+        
+        
+        start_lat = 40.677537
+        return render_template('home.html', form=form, answer=True,
+                               start_station=start_station, end_station = end_station,
+                               start_lat=start_lat)
+    return render_template('home.html', form=form, answer='none')
 
 @bp.route('/aboutme')
 def aboutme():
     return render_template('aboutme.html')
-
-@bp.route('/categories')
-def categories():
-    return render_template('categories.html')
-
-@bp.route('/posts')
-def posts():
-    return render_template('posts.html')
-
-@bp.route('/posts/<year>/<month>/<date>/<title>')
-def blogpost(year, month, date, title):
-    if title.endswith('.html') == False:
-        title += '.html'
-    url = os.path.join('posts', year, month, date, title)
-    return render_template(url)
-
 
 class ExampleForm(FlaskForm):
     dt = DateField('DatePicker', format='%Y-%m-%d',
@@ -65,38 +77,3 @@ class ExampleForm(FlaskForm):
     end_long = FloatField('End Longitude',
                           default=-73.9885504,
                           validators=[DataRequired()])
-
-from citibike_only_copy import SearchRoutes
-    
-@bp.route('/query_map', methods=['POST','GET'])
-def query_map():
-
-    form = ExampleForm()
-    if form.validate_on_submit():
-        """
-        sr = SearchRoutes()
-        r = sr.start_to_end([form.start_lat.data,
-                             form.start_long.data],
-                             [form.end_lat.data,
-                              form.end_long.data])
-
-
-        inds =  sr.stations['id'] == r[1]
-
-        answer = 'Walk to the citibike station at {}.\n'.format(sr.stations[inds].stationName.values[0])
-        inds = sr.stations['id'] == r[2]
-        answer += 'Bike to the citibike station at {}.\n'.format(sr.stations[inds].stationName.values[0])
-        answer += "Continue to your final location."
-        """
-        answer = """
-        Walk to the citibike station at Classon Ave & St Marks Ave. 
-Bike to the citibike station at Broadway & W 41 St. Continue to your final location.
-        """
-        start_station = "Classon Ave & St Marks Ave."
-        end_station = "Broadway & W 41 St."
-        start_lat = 40.677537
-        return render_template('query_map.html', form=form, answer=answer,
-                               start_station=start_station, end_station = end_station,
-                               start_lat=start_lat)
-    return render_template('query_map.html', form=form, answer='none')
-    
